8bc33956813df00d829b56eaf086e423a91fc20a
Valentin Chystiakov <>
2023-07-06 20:05:54 +0300

    Last Update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,32 @@
 // import React module
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import styles
 import './Form.css';
+// import useTelegram module
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
   // Creating state management for form variables
   const [Country, setCountry] = useState('');
   const [Street, setStreet] = useState('');
   const [subject, setSubject] = useState('physical');
+  const { tg } = useTelegram();
+
+  // Switching text inside the button
+  useEffect(() => {
+    tg.mainButton.setParams({
+      text: 'Send Data',
+    });
+  }, []);
+
+  // switching visibility of button depending on poles
+  useEffect(() => {
+    if (!Street || !Country) {
+      tg.mainButton.hide();
+    } else {
+      tg.mainButton.show();
+    }
+  }, [Country, Street]);
 
   //Functions that will react on EventListeners onChange
   const onChangeCountry = (e) => {
